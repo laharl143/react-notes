98efe196d7c346c380ce45e27dbb54acb19866e8
unknown <erskine.duenas@gmail.com>
2022-09-06 11:14:45 +0800

    updated boiler plate

--- boiler-plate-index.js
+++ boiler-plate-index.js
@@ -6,8 +6,10 @@ const number = "7";
 
 ReactDOM.render(
   <div>
-    <h1>Hello {name}!</h1>
+    <h1>Hello {name}!</h1>                    //you can add any javascript "expression" inside a curly bracket, but not javascript statement. expression is something that gives a value while statement asks the computer to do some work
     <p>Your lucky number is {number}</p>
   </div>,
   document.getElementById("root")
 );
+
+
