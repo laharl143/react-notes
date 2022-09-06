60bac65700ae44782b1f444868f0cab9648c285c
unknown <erskine.duenas@gmail.com>
2022-09-06 09:46:42 +0800

    added some notes

---
+++ notes.js
@@ -0,0 +1,19 @@
+Important Keywords:
+
+JSX 
+Props 
+Styling 
+Destructuring 
+ES6 
+Declarative Programming 
+Hooks 
+Spread Operator 
+Import 
+Export 
+Components 
+Use State 
+Babel 
+State 
+Map & Filter 
+Virtual DOM 
+Container
