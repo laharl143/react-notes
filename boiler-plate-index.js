c03d78f804e8230ab90b3522da88baf578fd6bdc
unknown <erskine.duenas@gmail.com>
2022-09-06 12:20:24 +0800

    added some notes

--- boiler-plate-index.js
+++ boiler-plate-index.js
@@ -1,15 +1,43 @@
+//codesandbox.io
+
 import React from "react";
 import ReactDOM from "react-dom";
 
-const name = "Erskine";
+const fName = "Erskine";
+const lName = "Duenas";
 const number = "7";
 
 ReactDOM.render(
   <div>
-    <h1>Hello {name}!</h1>                    //you can add any javascript "expression" inside a curly bracket, but not javascript statement. expression is something that gives a value while statement asks the computer to do some work
+    <h1>Hello {fName + " " + lName}!</h1>                    //you can add any javascript "expression" inside a curly bracket, but not javascript statement. expression is something that gives a value while statement asks the computer to do some work
     <p>Your lucky number is {number}</p>
   </div>,
   document.getElementById("root")
 );
 
 
+///////////////////////////////////////////////////////////////////////////////
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+const yourName = "Erskine Duenas";
+const currentDate = new Date();
+const year = currentDate.getFullYear();
+
+
+ReactDOM.render(
+  <div>
+    <p>Created by {yourName}</p>                   
+    <p><i>Copyright {year}</i></p> 
+  </div>,
+  document.getElementById("root")
+);
+//Create a react app from scratch.
+//It should display 2 paragraph HTML elements.
+//The paragraphs should say:
+//Created by YOURNAME.
+//Copyright CURRENTYEAR.
+//E.g.
+//Created by Angela Yu.
+//Copyright 2019.
